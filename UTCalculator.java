bd7d1695164572ab45ea8344a2d2cc7ade198fe6
Antosh <antosh.dyade@gmail.com>
2024-04-15 13:36:17 +0530

    UTCalculator.java  : JUnit  Example Code
    JUnit is a software testing framework that helps developers test their applications

---
+++ UTCalculator.java
@@ -0,0 +1,11 @@
+package amd;
+
+public class UTCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
