48cd4c746c2089dc7a171e8d47c393d88f0d8c7e
Antosh <antosh.dyade@gmail.com>
2024-04-15 15:08:00 +0530

    Example for FileInputStream: Demonstrating All 3types of Read, available and skip methods

---
+++ InputStream1.java
@@ -0,0 +1,47 @@
+package SY2024;
+import java.io.*;
+
+public class InputStream1 {
+
+	public static void main(String[] args) {
+	try
+		{
+		FileInputStream fis=new FileInputStream("E://Java/MyFile.txt");
+		
+		//Method 1
+		int x;
+		
+		while((x=fis.read())!=-1) {
+			
+			System.out.print((char)x);
+		}
+		
+		
+      //Method 2
+		
+		/*byte b[]=new byte[fis.available()];
+		fis.read(b);
+		String msg=new String(b);
+		System.out.println(msg);
+			*/
+	
+      
+      //Method 3
+	
+		/*fis.skip(5); //Skipping first 5 Character
+		byte b[]=new byte[fis.available()];
+		//fis.read(b);
+		fis.read(b,0,b.length);
+		String msg=new String(b);
+		System.out.println(msg);
+		*/
+		
+		
+	}catch (FileNotFoundException e) {
+		System.out.println(e);
+	}catch (IOException e) {
+		System.out.println(e);
+	}
+	
+	}
+}
