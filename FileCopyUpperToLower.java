ea84f85c98f8ca5a7d1eeed14576c069a260af55
Antosh <antosh.dyade@gmail.com>
2024-04-16 20:41:39 +0530

    Update FileCopyUpperToLower.java

--- FileCopyUpperToLower.java
+++ FileCopyUpperToLower.java
@@ -5,13 +5,27 @@ public class FileCopyUpperToLower {
 	public static void main(String[] args) throws Exception {
 		FileInputStream fis=new FileInputStream("D:/JavaCode/in.txt");
 		FileOutputStream fos=new FileOutputStream("D:/JavaCode/out.txt");
-
-		int x;
+		
+		//Method 1
+	/*	int x;
 		while((x=fis.read())!=-1) {
 			if (x>=65 && x<=90)fos.write(x+32);
 			else
 				fos.write(x);
 		}
-	}
+		
+		*/
+		
+		//Method 2
+		 int x;
+         while ((x = fis.read()) != -1) {
+             if (Character.isUpperCase(x)) {
+                 x = Character.toLowerCase(x);
+             }
+             fos.write(x);
+         }
 
+         
+       
+	}
 }
