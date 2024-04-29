712dd880ccbf8b3759b3c61c883d1d976eb69734
Antosh <antosh.dyade@gmail.com>
2024-04-29 11:38:19 +0530

    Update OperationOnMapCollectionFrameWork.java

--- OperationOnMapCollectionFrameWork.java
+++ OperationOnMapCollectionFrameWork.java
@@ -1,7 +1,6 @@
 package amd;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class OperationOnMapCollectionFrameWork {
@@ -9,7 +8,7 @@ public class OperationOnMapCollectionFrameWork {
         // Creating two Maps
         Map<Integer, Integer> map1 = new HashMap<>();
         Map<Integer, Integer> map2 = new HashMap<>();
-        
+
         // Adding elements to the first Map
         map1.put(0, 10);
         map1.put(1, 2);
@@ -32,15 +31,20 @@ public class OperationOnMapCollectionFrameWork {
         map1.put(2, 18);
         System.out.println("After Adding Element at key 2: " + map1);
 
-        // Sorting the Elements (Maps don't maintain order, so no explicit sorting needed)
+        // Retrieving value associated with a specific key
+        int valueForKey2 = map1.get(2);
+        System.out.println("Value for key 2: " + valueForKey2);
+
         // Searching for key 2
         boolean found = map1.containsKey(2);
         System.out.println("Key 2 Found: " + found);
-        
-        boolean foundvalue = map1.containsValue(13);
-        if(foundvalue) System.out.println("The Value 13 Found");
-        else System.out.println("The Value 13 Not Found");
-        
+
+        // Checking if value 13 exists in the map
+        boolean foundValue13 = map1.containsValue(13);
+        if (foundValue13)
+            System.out.println("The Value 13 Found");
+        else
+            System.out.println("The Value 13 Not Found");
 
         // Removing elements from map1
         map1.remove(1); // Remove the element with key 1
